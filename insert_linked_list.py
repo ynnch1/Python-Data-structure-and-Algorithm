2a24629ccbe41ecbef7939845a30e33d4c75a441
ynnch1 <mixhii.ko@gmail.com>
2022-04-02 12:12:27 +0700

    insert data into linked list

---
+++ insert_linked_list.py
@@ -0,0 +1,40 @@
+# create new node class to define node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# create linked list class which contain its behavior such push, append, pirnt out the linked list data
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def append(self, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node 
+            return
+
+            last = self.head
+            while(last, next):
+                last = last.next
+            last.next = mew_mode
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.append(6)
+    llist.push(7)
+    llist.push(1)
+
+    llist.printList()
