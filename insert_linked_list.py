d541f096c9e0920ca79163f9b0cb5ea20faf2d13
ynnch1 <mixhii.ko@gmail.com>
2022-04-03 10:28:33 +0700

    linked list with comment

--- insert_linked_list.py
+++ insert_linked_list.py
@@ -8,12 +8,12 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-
+#make the new node as the head
     def push(self, new_data):
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
-
+#
     def append(self, new_data):
         new_node = Node(new_data)
         if self.head is None:
@@ -24,13 +24,13 @@ class LinkedList:
             while(last, next):
                 last = last.next
             last.next = mew_mode
-
+#print
     def printList(self):
         temp = self.head
         while(temp):
             print(temp.data, end=" ")
             temp = temp.next
-
+#code execution
 if __name__ == '__main__':
     llist = LinkedList()
     llist.append(6)
